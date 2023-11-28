70729f84331dbbad7f3f5c6f855f2f48bbd0d732
TimohinA <fortran307@gmail.com>
2023-11-28 22:36:12 +0300

    visualization full workbook

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -3,17 +3,25 @@
     <div class="container mt-5" v-if="data.length === 0">
       <formInput @load-file="handleFileUpload" />
     </div>
-
+    
     <div v-else>
-      <listThemes :data="data" @selectQuestion="selectQuestion" />
+      <ul class="nav nav-tabs justify-content-end">
+        <li class="nav-item" v-for="sheet in data">
+          <button 
+            class="nav-link" 
+            :class="currentSheet === data.indexOf(sheet)? 'active' : ''"
+            @click.prevent="changeSheet(data.indexOf(sheet))"  
+          >{{ sheet.sheet }}</button>
+        </li>
+      </ul>
+      <listThemes :data="dataSheet" @selectQuestion="selectQuestion" />
       <modalWindow :currentQuestion="selectedQuestion">
-        <chart v-if="chartData" :chart-data="chartData"/>
+        <chart v-if="chartData" :chart-data="chartData" />
       </modalWindow>
     </div>
   </div>
 </template>
 <!-- Мы ничего не передаем на начальном этапе на отрисовку. Без v-if получим ошибку от ChartJS -->
-<!-- ChartJS динамически перерисовывает картнку благодаря миксину от vue-ChartJS (иначе он не перерисовал бы) -->
 
 <script>
 import axios from "axios";
@@ -22,18 +30,28 @@ import listThemes from "./components/ListThemes.vue";
 import modalWindow from "./components/ModalWindow.vue";
 import chart from "./components/Chart.vue";
 
-import colorLine from "@/data/colorLine";
-
 export default {
   components: { formInput, modalWindow, chart, listThemes },
   data() {
     return {
       data: new Array(),
       selectedQuestion: null,
-      chartData: null,
-    };
+      chartData: {
+        labels: new Array(),
+        datasets: new Array(),
+      },
+      currentSheet: 0
+    }
+  },
+  computed: {
+    dataSheet(){
+      return this.data[this.currentSheet].data
+    }
   },
   methods: {
+    changeSheet(newSheet){
+      this.currentSheet = newSheet
+    },
     async handleFileUpload(file) {
       let formData = new FormData();
       formData.append("table", file);
@@ -54,7 +72,6 @@ export default {
       }
     },
     selectQuestion(details) {
-      console.log(details.answers[0]);
       this.selectedQuestion = details.question;
       this.chartData = {
         labels: details.answers[0].history.map((item) => item.date),
@@ -62,9 +79,6 @@ export default {
           return {
             label: element.answer,
             data: element.history.map((item) => item.percentage),
-            backgroundColor: colorLine[index].backgroundColor,
-            borderColor: colorLine[index].borderColor,
-            fill: false,
           };
         }),
       };
