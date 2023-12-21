de005da838947beab287942f889968abfd488e20
Timohin_Alex <fortran304@yandex.ru>
2023-12-21 17:25:17 +0300

    fixed the length of the labels on the chart

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div class="container-fluid bg-body mt-3">
+  <div class="container-fluid bg-body">
+    <loadWindow v-if="loadData" />
     <div class="container mt-5" v-if="data.length === 0">
+      <mainPage />
+
+      <modalWindow :title="`Как пользоваться Графиком?`">
+        <manualPage />
+      </modalWindow>
+
       <formInput @load-file="handleFileUpload" />
-      <spinner />
     </div>
-    
+
     <div v-else>
-      <listSheets :sheets="data" :currentSheet="currentSheet" @changeSheet="changeSheet"/>
+      <listSheets
+        :sheets="data"
+        :currentSheet="currentSheet"
+        @changeSheet="changeSheet"
+      />
       <listThemes :data="dataSheet" @selectQuestion="selectQuestion" />
-      <modalWindow :currentQuestion="selectedQuestion">
+
+      <modalWindow :title="selectedQuestion">
         <chart v-if="chartData" :chart-data="chartData" />
       </modalWindow>
     </div>
   </div>
 </template>
-<!-- Мы ничего не передаем на начальном этапе на отрисовку. Без v-if получим ошибку от ChartJS -->
 
 <script>
-import axios from "axios";
-import formInput from "./components/FormInput";
-import listThemes from "./components/ListThemes.vue";
-import modalWindow from "./components/ModalWindow.vue";
-import listSheets from "./components/ListSheets.vue";
-import chart from "./components/Chart.vue";
-import spinner from "./components/Spinner.vue"
+import axios from "axios"
+import formInput from "./components/FormInput"
+import listThemes from "./components/ListThemes.vue"
+import modalWindow from "./components/ModalWindow.vue"
+import listSheets from "./components/ListSheets.vue"
+import loadWindow from "./components/LoadWindow.vue"
+import manualPage from "./page/ManualPage.vue"
+import mainPage from "./page/FirstPage.vue"
+import chart from "./components/Chart.vue"
 
 export default {
-  components: { formInput, modalWindow, chart, listThemes, listSheets, spinner },
+  components: {
+    formInput,
+    modalWindow,
+    chart,
+    listThemes,
+    listSheets,
+    loadWindow,
+    manualPage,
+    mainPage,
+  },
   data() {
     return {
       data: new Array(),
@@ -35,21 +56,23 @@ export default {
         labels: new Array(),
         datasets: new Array(),
       },
-      currentSheet: 0
-    }
+      currentSheet: 0,
+      loadData: false,
+    };
   },
   computed: {
-    dataSheet(){
+    dataSheet() {
       return this.data[this.currentSheet].data
-    }
+    },
   },
   methods: {
-    changeSheet(newSheet){
+    changeSheet(newSheet) {
       this.currentSheet = newSheet
     },
     async handleFileUpload(file) {
-      let formData = new FormData();
-      formData.append("table", file);
+      this.loadData = true
+      let formData = new FormData()
+      formData.append("table", file)
 
       try {
         const resFile = await axios.post(
@@ -57,20 +80,26 @@ export default {
           formData,
           {
             headers: {
-              "Content-Type": "multipart/form-data",
-            },
+              "Content-Type": "multipart/form-data"
+            }
           }
-        );
+        )
         this.data = resFile.data
+        setTimeout(() => {
+          this.loadData = false
+        }, 1500);
       } catch (error) {
         console.error(error.message)
       }
     },
     selectQuestion(details) {
-      this.selectedQuestion = details.question;
+      // Проходим по всем массивам ответов и находим самый длинный массив
+      const longLabels = details.answers.reduce((acc, curr) => curr.history.length > acc.history.length ? curr : acc)
+
+      this.selectedQuestion = details.question
       this.chartData = {
-        labels: details.answers[0].history.map((item) => item.date),
-        datasets: details.answers.map((element, index) => {
+        labels: longLabels.history.map((item) => item.date),
+        datasets: details.answers.map((element) => {
           return {
             label: element.answer,
             data: element.history.map((item) => item.percentage),
---
+++ Client/src/components/LoadWindow.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="loading-overlay">
+    <div class="d-flex flex-column align-items-center">
+      <span class="fw-bold text-white mb-3">Идет конвертация данных...</span>
+      <span class="loader"></span>
+    </div>
+  </div>
+</template>
+
+<style>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+}
+
+.loader {
+  position: relative;
+  width: 100px;
+  height: 16px;
+}
+
+.loader:before,
+.loader:after {
+  content: "";
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #fff;
+  box-shadow: 32px 0 #fff;
+  left: 0;
+  top: 0;
+  animation: ballMoveX 1s linear infinite;
+}
+
+.loader:after {
+  box-shadow: none;
+  transform-origin: 40px 0;
+  transform: rotate(-153deg);
+  animation: rotateLoader 1s linear infinite;
+}
+
+@keyframes rotateLoader {
+  0%,
+  10% {
+    transform: rotate(-153deg);
+  }
+  90%,
+  100% {
+    transform: rotate(0deg);
+  }
+}
+
+@keyframes ballMoveX {
+  0%,
+  10% {
+    transform: translateX(0)
+  }
+  90%,
+  100% {
+    transform: translateX(32px)
+  }
+}
+</style>
